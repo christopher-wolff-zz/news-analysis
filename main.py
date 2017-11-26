b81e43f201dd81f477a712940b82e9b47fba76c1
Christopher Wolff <cfw13@duke.edu>
2017-11-26 12:02:48 -0500

    Implement automatic querying and json exports

--- main.py
+++ main.py
@@ -13,19 +13,43 @@ BASE_URI = 'http://api.nytimes.com/svc/mostpopular/v2'
 TYPE = 'mostviewed'
 SECTION = 'all-sections'
 TIME_PERIOD = '1'
+RESPONSE_FORMAT = 'json'
 
 
-def main():
-    """Do things."""
+def query(num_queries=1):
+    """Request data from NYT and store it as a json file.
+
+    Args:
+        num_queries (int): The number of queries
+
+    """
     # Load settings
     data = json.load(open('settings.json'))
     API_KEY = data['API_KEY']
 
-    # Send request
-    URI = f'{BASE_URI}/{TYPE}/{SECTION}/{TIME_PERIOD}.json?api_key={API_KEY}'
-    r = requests.get(URI)
-    pprint(r.json())
+    # Send requests
+    URI = f'{BASE_URI}/{TYPE}/{SECTION}/{TIME_PERIOD}.{RESPONSE_FORMAT}'
+    results = []
+    for k in range(num_queries):
+        offset = k * 20
+        payload = {'api_key': API_KEY, 'offset': offset}
+        response = requests.get(URI, params=payload)
+        results += response.json()["results"]
+
+    # Save to file
+    with open('data/mostviewed.json', 'w') as output_file:
+        json.dump(results, output_file)
+
+
+class Article():
+    """A representation of an article."""
+
+    def __init__(self):
+        """Initialize the article."""
+        pass
 
 
 if __name__ == '__main__':
-    main()
+    query(10)
+    data = json.load(open('data/mostviewed.json'))
+    pprint(len(data))
