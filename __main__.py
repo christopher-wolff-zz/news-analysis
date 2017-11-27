f9879da229129876a5fd942212b43ee401ffaa85
Christopher Wolff <cfw13@duke.edu>
2017-11-27 00:15:40 -0500

    Clean up

--- __main__.py
+++ __main__.py
@@ -11,10 +11,8 @@ import urllib
 
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
-import numpy as np
 # import nltk
 # from nltk.tokenize import sent_tokenize
-# from pprint import pprint
 import requests
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
@@ -37,63 +35,75 @@ def query(num_queries=1):
     # Load settings
     settings = json.load(open('settings.json'))
     API_KEY = settings['API_KEY']
-
     # Send requests
     URI = f'{BASE_URI}/{TYPE}/{SECTION}/{TIME_PERIOD}.{RESPONSE_FORMAT}'
-    results = []
+    articles = []
     for k in range(num_queries):
         print(f'Running query {k+1}...')
         offset = k * 20
         payload = {'api_key': API_KEY, 'offset': offset}
         response = requests.get(URI, params=payload)
-        results += response.json()['results']
-
+        articles += response.json()['results']
     # Save to file
     with open('data/raw.json', 'w') as output_file:
-        json.dump(results, output_file)
+        json.dump(articles, output_file)
+
+
+def get_stories():
+    """Get full document texts from urls."""
+    articles = json.load(open('data/raw.json'))
+    for k, article in enumerate(articles):
+        print(f'Analyzing article {k+1}...')
+        url = article['url']
+        f = urllib.request.urlopen(url)
+        soup = BeautifulSoup(f, 'html5lib')
+        story = ''
+        for par in soup.find_all('p', class_='story-body-text story-content'):
+            if par.string:
+                story += ' ' + par.string
+        article.update({'story': story})
+    # Save to file
+    with open('data/sentiments.json', 'w') as output_file:
+        json.dump(articles, output_file)
 
 
 def analyze():
     """Analyze gathered data."""
     # Calculate sentiment scores
-    results_ = json.load(open('data/raw.json'))
-    for k, result in enumerate(results_):
-        title = result['title']
-        abstract = result['abstract']
-        url = result['url']
+    articles = json.load(open('data/stories.json'))
+    for k, article in enumerate(articles):
+        print(f'Analyzing article {k+1}...')
+        title = article['title']
+        abstract = article['abstract']
+        story = article['story']
 
-        f = urllib.request.urlopen(url)
-        soup = BeautifulSoup(f, 'html5lib')
-        document = ''
-        for par in soup.find_all('p', class_='story-body-text story-content'):
-            if par.string:
-                document += par.string
-
-        title_blob = TextBlob(title)
-        abstract_blob = TextBlob(abstract)
-        document_blob = TextBlob(document)
+        title_blob = TextBlob(title, analyzer=NaiveBayesAnalyzer())
+        abstract_blob = TextBlob(abstract, analyzer=NaiveBayesAnalyzer())
+        story_blob = TextBlob(story, analyzer=NaiveBayesAnalyzer())
 
         title_sent = title_blob.sentiment
         abstract_sent = abstract_blob.sentiment
-        document_sent = document_blob.sentiment
+        story_sent = story_blob.sentiment
 
-        result.update({'title_sent': title_sent,
-                       'abstract_sent': abstract_sent,
-                       'document_sent': document_sent})
-        print(f'{k+1}: {title} {title_sent.polarity} {abstract_sent.polarity} {document_sent.polarity}')
+        article.update({'title_sent': title_sent,
+                        'abstract_sent': abstract_sent,
+                        'story_sent': story_sent})
+
+        print(f'{k+1}: {title} {title_sent.p_pos} {abstract_sent.p_pos} \
+              {story_sent.p_pos}')
 
     # Save to file
     with open('data/sentiments.json', 'w') as output_file:
-        json.dump(results_, output_file)
+        json.dump(articles, output_file)
 
 
 def visualize():
     """Visualize the data."""
     # Load data
-    data = json.load(open('data/sentiments.json'))
-    title_sents = [result['title_sent'][0] for result in data]
-    abstract_sents = [result['abstract_sent'][0] for result in data]
-    doc_sents = [result['abstract_sent'][0] for result in data]
+    articles = json.load(open('data/sentiments.json'))
+    title_sents = [article['title_sent'][0] for article in articles]
+    abstract_sents = [article['abstract_sent'][0] for article in articles]
+    story_sents = [article['story_sent'][0] for article in articles]
 
     # Plot data
     plt.figure(1)
@@ -107,8 +117,8 @@ def visualize():
     plt.xlabel('Rank')
     plt.ylabel('Sentiment')
     plt.figure(3)
-    plt.stem(range(1, len(doc_sents)+1), doc_sents)
-    plt.title('Document Sentiment')
+    plt.stem(range(1, len(story_sents)+1), story_sents)
+    plt.title('Story Sentiment')
     plt.xlabel('Rank')
     plt.ylabel('Sentiment')
     plt.show()
@@ -116,5 +126,6 @@ def visualize():
 
 if __name__ == '__main__':
     # query(100)
-    # analyze()
-    visualize()
+    # get_stories()
+    analyze()
+    # visualize()
