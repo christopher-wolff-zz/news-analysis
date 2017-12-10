445db0ce10bb272246ef1674ccea1f185787ec87
Christopher Wolff <cfw13@duke.edu>
2017-12-10 17:33:36 -0500

    Add sample usage script

--- __main__.py
+++ __main__.py
@@ -1,8 +1,19 @@
-"""Main module."""
+"""Sample use of analyzer module."""
 
-from analyzer import Analyzer
+from analyzer import query, scrape_stories
 
 
 if __name__ == '__main__':
-    a = Analyzer()
-    a.label_articles()
+    print('Requesting articles from NYT API')
+    print('================================')
+    query(num_queries=10)
+    print()
+
+    print('Scraping article texts from NYT website')
+    print('=======================================')
+    scrape_stories()
+    print()
+
+    print('Training classifiers')
+    print('====================')
+    train_model()
